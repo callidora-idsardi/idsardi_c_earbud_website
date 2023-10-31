8501bc81a29ce9e637d5167fe55ff33d8f103f3a
callidora-idsardi <114117190+callidora-idsardi@users.noreply.github.com>
2023-10-31 06:43:40 -0400

    added base folders and files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# idsardi_c_earbud_website
+## <img src="images/hotspot.svg" alt="Crystal Icon" width="15px">  Crystal ClearBuds | Augmented Reality  <img src="images/hotspot.svg" alt="Crystal Icon" width="15px"> 
+
+Welcome to Crystal ClearBuds in agumented reality! 
+
+Hover over each hotspot to discover more information about each part.
+
+
+## Process  <img src="images/hotspot1-music.svg" alt="Crystal Icon" width="15px"> 
+- Made with Cinema 4D & exported as a GLTF file into Model Veiwer
+- Integrated AR Object into Crystal ClearBud's website
+- Used JavaScript & Greensock to dynamically display hotspot information & create hover effects
+
+## Contributing  <img src="images/hotspot1-music.svg" alt="Crystal Icon" width="15px"> 
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Credits  <img src="images/hotspot1-music.svg" alt="Crystal Icon" width="15px"> 
+Callidora Idsardi 
+
+## License  <img src="images/hotspot1-music.svg" alt="Crystal Icon" width="15px"> 
+MIT
---
+++ js/main.js
@@ -0,0 +1,102 @@
+(() => {
+    console.log("IIFE Fired");
+
+    const model = document.querySelector("#model");
+    const hotSpots = document.querySelectorAll(".Hotspot");
+
+    const infoSpots = [
+
+        {
+            title: "Earbud Speaker",
+            desc: "Crystal Clear speaker technology - conversation-amplify abilities while filtering unwanted sounds. Play music undisturbed!",
+            image:{
+                src:"images/hotspot1-music.svg",
+                alt:"music note"
+            } 
+        },
+
+        {
+            title: "Outer Mic",
+            desc: "Two outer microphones ensures Crystal Clear calls with minimal interruptions.",
+            image:{
+                src:"images/hotspot3-convo.svg",
+                alt:"conversation bubble"
+            } 
+        },
+
+        {
+            title: "Volume & Call Control",
+            desc: "Control the volume by sliding up or down. Call control capabilities upon pressing twice on the slider. Refer to the Crystal ClearBuds instructions for more information.",
+            image:{
+                src:"images/hotspot5-calls.svg",
+                alt:"cell phone"
+            } 
+        },
+
+        {
+            title: "Spatial Vent",
+            desc: "Noise canceling spatial vent at the back of the earbud filters out unwanted noises while letting in ambient sounds.",
+            image:{
+                src:"images/hotspot4-alert.svg",
+                alt:"exclamation alert bubble"
+            } 
+        },
+
+     
+        {
+            title: "Eartip Size M",
+            desc: "Get the perfect fit with a selection of earbud sizes - S, M & L silicone tip sizes available.",
+            image:{
+                src:"images/hotspot2-size.svg",
+                alt:"size arrows"
+            } 
+            
+        }
+
+    ];
+
+
+    function loadSpots() {
+    infoSpots.forEach((info, index) => {
+        let hotspotCon = document.querySelector(`#hotspot-${index + 1}`);
+        const spotTitle = document.createElement("h2");
+        const spotDesc = document.createElement("p");
+        const spotImage = document.createElement("img");
+
+        spotTitle.textContent = info.title;
+        hotspotCon.appendChild(spotTitle);
+
+        spotDesc.textContent = info.desc;
+        hotspotCon.appendChild(spotDesc);
+
+        spotImage.src = info.image.src;
+        spotImage.alt = info.image.alt;
+        hotspotCon.appendChild(spotImage);
+    });
+    }   
+
+    loadSpots();
+
+    function loaded() {
+        hotSpots.forEach(hotSpot => {
+            hotSpot.style.display = "block";
+        });
+    }
+
+    function displayInfo() {
+        let selected = document.querySelector(`#${this.slot}`);
+        gsap.to(selected, 1, { autoAlpha: 1 });
+    }
+
+    function hideInfo() {
+        let selected = document.querySelector(`#${this.slot}`);
+        gsap.to(selected, 1, { autoAlpha: 0 });
+    }
+
+        model.addEventListener("load", loaded);
+        hotSpots.forEach(function (hotspot) {
+        hotspot.addEventListener("mouseover", displayInfo);
+        hotspot.addEventListener("mouseout", hideInfo);
+    });
+})();
+
